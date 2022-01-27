304330687da8adb8d2bd65504cf28c2d8c3e79ed
Lesther Caballero <itslesther@Lesthers-MacBook-Pro.local>
2022-01-27 06:13:50 -0300

    port fixed

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Database } from './Database';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 
   const database = new Database();
   database.init();
